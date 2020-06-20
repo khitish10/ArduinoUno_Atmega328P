eb9beb4fb8c8bf65b7d8ab2ec7c3f5b5c1743e72
khitish10 <khitish10@gmail.com>
2020-06-19 20:29:31 -0700

    Add files via upload

---
+++ Line_Follower/README.md
@@ -0,0 +1,12 @@
+# Line-Follower
+#This is a basic line follower running on white base and black tracks.
+#Parts:-
+i.    chasis
+ii.   wheels and caster wheel
+iii.  arduino uno
+iv.   motor driver L293D
+v.    male-male and male-female jumper wires
+vi.   8 array IR Sensor
+vii.  double sided tape and black tape(as per requirement)
+viii. 12volt adapter for power supply
+ix.   200 or 300 rpm centre shaft dc motor(no. of motors as per the requirement)
---
+++ Line_Follower/arduinolinefollower1.ino
@@ -0,0 +1,46 @@
+int switchstate=0;
+int motorpin1 =10;
+int motorpin2 = 11;
+int moterpin3 = 6;
+int motorpin4 = 9;
+int enablepin = 5;
+int enablepin2 = 3;
+int sensor = 7;
+int sensor2 = 8;
+int sensorstate1 = 0;
+int sensorstate2 = 0;
+void setup()
+{
+pinMode (7, INPUT);
+pinMode (10, OUTPUT);
+pinMode (11, OUTPUT);
+pinMode(enablepin,OUTPUT); pinMode (8, INPUT);
+pinMode (6, OUTPUT);
+pinMode (9, OUTPUT); }
+void loop()
+{ 
+digitalWrite(enablepin,HIGH); digitalWrite(enablepin2,HIGH);
+sensorstate1= digitalRead(7);
+sensorstate2= digitalRead(8);
+if(sensorstate1 == HIGH)
+{
+analogWrite(10,80);
+analogWrite(11,0); }
+else
+{
+analogWrite(10,0);
+analogWrite(11,0);
+
+}
+if(sensorstate2 == HIGH)
+{
+analogWrite(6,80);
+analogWrite(9,0); }
+else
+{
+analogWrite(6,0);
+analogWrite(9,0);
+
+}
+
+}
